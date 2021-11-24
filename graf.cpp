3f1bbe90eb5ddeca0f31b0b2aaf002224d29d5cf
thatnickk <62030882+thatnickk@users.noreply.github.com>
2021-11-24 14:26:18 +0200

    updated class

--- graf.cpp
+++ graf.cpp
@@ -4,18 +4,24 @@
 #include <queue>
 #include <stack>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
-ifstream fin("ctc.in");
-ofstream fout("ctc.out");
+const int inf = INT_MAX;
+
+ifstream fin("bellmanford.in");
+ofstream fout("bellmanford.out");
 
 class Graf{
+
     private:
         bool orientat;
+        bool ponderat;
         int nrNoduri;
         int nrMuchii;
         vector<vector<int>> listaAdiacenta;
+        vector<pair<int, pair<int, int>>> listaMuchii;
         vector<int> topologie;
 
         void bfs(vector<int>& distante, vector<bool>& vizitat, int nodStart){
@@ -53,19 +59,19 @@ class Graf{
             for (auto i : listaAdiacenta[nodCurent]){
                 if (i != nodAnterior){
                     if (vizitat[i] == true){
-                        adancimeMin[nodCurent] = min(adancimeMin[nodCurent], adancime[i]);
+                        adancimeMin[nodCurent] = min(adancimeMin[nodCurent], adancime[i]); //gasim muchie de intoarcere
                     }
                     else{
-                        dfsbiconex(i, nodCurent, adancimeCurenta + 1, componente, stiva, vizitat, adancime, adancimeMin);
+                        dfsbiconex(i, nodCurent, adancimeCurenta + 1, componente, stiva, vizitat, adancime, adancimeMin); //rulam la adancime +1 cu vecinul
                         adancimeMin[nodCurent] = min(adancimeMin[nodCurent], adancimeMin[i]);
-                        if(adancime[nodCurent] <= adancimeMin[i]){
+                        if(adancime[nodCurent] <= adancimeMin[i]){  // daca e adevarat am gasit un nod care desparte doua componente biconexe
                             vector<int> componenta;
                             componenta.push_back(nodCurent);
                             int nod = stiva.top();
                             stiva.pop();
                             componenta.push_back(nod);
                             while (i != nod){
-                                nod = stiva.top();
+                                nod = stiva.top();  // dam pop din stiva pana la vecin
                                 stiva.pop();
                                 componenta.push_back(nod);
                             }
@@ -85,16 +91,16 @@ class Graf{
  
 	        for (auto i : listaAdiacenta[nodCurent])
 	        {
-		        if (ordineDescoperire[i] == -1)
+		        if (ordineDescoperire[i] == -1) //daca nu am vizitat, facem dfs pe nodul respectiv
 		        {
 			        TarjanDFS(i, componente, stiva, peStiva, ordineDescoperire, adancimeMin, timp);
 			        adancimeMin[nodCurent] = min(adancimeMin[nodCurent], adancimeMin[i]);
 		        }
-		        else if (peStiva[i] == true)
+		        else if (peStiva[i] == true) //daca e pe stiva atunci face parte dintr-o componenta tare conexa
 			        adancimeMin[nodCurent] = min(adancimeMin[nodCurent], ordineDescoperire[i]);
 	        }
  
-	        if (adancimeMin[nodCurent] == ordineDescoperire[nodCurent])	
+	        if (adancimeMin[nodCurent] == ordineDescoperire[nodCurent])	 //daca sunt egale inseamna ca nodCurent este punct de inceput pt o componenta
 	        {
 		        vector <int> componenta;
 		        int nod = stiva.top();
@@ -142,25 +148,47 @@ class Graf{
         //     }
         // } nu merge
 
-    public:
-        Graf(){
-            nrNoduri = 0;
-            nrMuchii = 0;
-            orientat = false;
-            vector<int> gol;
-            for (int i = 0; i < nrNoduri; i++)
-                listaAdiacenta.push_back(gol);
+        int cautaRoot(int nod, vector<int> tati){
+            while (nod != tati[nod])
+                nod = tati[nod];
+            return nod;
         }
-        Graf(int nrNod, int nrMuc, bool esteOrientat){
+
+        void combina(int tatax, int tatay, vector<int>& tati, vector<int>& inaltimi){
+            if(inaltimi[tatax] > inaltimi[tatay])
+                tati[tatay] = tatax;
+            else if (inaltimi[tatax] < inaltimi[tatay])
+                tati[tatax] = tatay;
+            else{
+                tati[tatax] = tatay;
+                inaltimi[tatay]++;
+            }
+        }
+
+    public:
+        Graf(int nrNod, int nrMuc, bool esteOrientat, bool estePonderat){
             nrNoduri = nrNod;
             nrMuchii = nrMuc;
-            orientat = esteOrientat;            
+            orientat = esteOrientat;
+            ponderat = estePonderat;            
             vector<int> gol;
-            for (int i = 0; i < nrNoduri; i++)
+            pair<int, pair<int, int>> gol2;
+            for (int i = 0; i <= nrMuchii; i++){
                 listaAdiacenta.push_back(gol);
+                listaMuchii.push_back(gol2);
+            }
+                
         }
 
         void setareGraf(){
+            if (ponderat){
+                int x, y, c;
+                for (int i = 1; i <= nrMuchii; i++){
+                    fin >> x >> y >> c;
+                    listaMuchii[i] = {c,{x, y}};
+                }
+            }
+            else{
             if (orientat){
                 int x, y;
                 for(int i = 0; i < nrMuchii; i++){
@@ -179,6 +207,7 @@ class Graf{
                     listaAdiacenta[x].push_back(y);
                     listaAdiacenta[y].push_back(x);
                 }
+            }
             }
         }
 
@@ -230,7 +259,6 @@ class Graf{
 			        TarjanDFS(i, componente, stiva, peStiva, ordineDescoperire, adancimeMin, timp);
 		        }
 	    }
- 
 	return componente;
 }
         void topologic(){
@@ -281,16 +309,146 @@ class Graf{
         //     return conexiuni;
         // } nu merge
 
+        void apmSiCost(){   //Kruskal
+            vector<pair<int,int>> apm;
+            vector<int> tati(nrNoduri + 1);
+            vector<int> inaltimi(nrNoduri + 1);
+            int cost = 0;
+            sort(listaMuchii.begin() + 1, listaMuchii.end()); //Sortam dupa cost muchiile
+            for (int i = 1; i <= nrNoduri; i++){
+                tati[i] = i;
+                inaltimi[i] = 0;
+            }
+
+            for (int i = 1; i <= nrMuchii; i++){
+                int tatax, tatay;
+                tatax = cautaRoot(listaMuchii[i].second.first, tati); //cautam radacinile nodurilor din muchia curenta
+                tatay = cautaRoot(listaMuchii[i].second.second, tati);
+
+                if (tatax != tatay){    //daca tatii sunt diferiti putem combina
+                    combina(tatax, tatay, tati, inaltimi);
+                    apm.push_back(listaMuchii[i].second);
+                    cost += listaMuchii[i].first;
+                }
+            }
+            int muchiiAPM = nrNoduri - 1;
+            fout<< cost << "\n" << muchiiAPM << "\n"; // nr muchii apm = nr noduri - 1
+            for (auto i : apm){
+                fout << i.first << " " << i.second << "\n"; 
+            } 
+        }
+
+        void disjoint(){
+            vector<int> tati(nrNoduri + 1);
+            vector<int> inaltimi(nrNoduri + 1, 0);
+            for (int i = 1; i <= nrNoduri; i++){
+                tati[i] = i;
+                //inaltimi[i] = 0;
+            }
+            
+            for (int i = 1; i <= nrMuchii; i++){
+                int operatie, x, y;
+                fin >> operatie >> x >> y;
+                int tatax = cautaRoot(x, tati);
+                int tatay = cautaRoot(y, tati);
+                if (operatie == 1)
+                    {   
+                        combina(tatax, tatay, tati, inaltimi);
+                    }
+                else if (tatax == tatay)    //daca au acelasi tata atunci sunt din aceeasi multime
+                    fout << "DA\n";
+                    else fout << "NU\n";
+            }
+        }
+
+	
+        vector<int> dijkstra() {
+            
+            vector<vector<pair<int, int>>> listaCosturi(nrNoduri + 1, vector<pair<int, int>>(1, {-1, -1}));
+            for (int i = 1; i <= nrMuchii; i++) {
+                listaCosturi[listaMuchii[i].second.first].push_back(make_pair(listaMuchii[i].second.second, listaMuchii[i].first));
+            }
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap; //heap ul folosit (pair: cost-nod destinatie)
+            vector<int> distanta(nrNoduri + 1, inf);
+            vector<int> vizitat(nrNoduri + 1, 0);
+            heap.push(make_pair(0, 1)); //punem in heap primul nod cu costul 0
+            distanta[1] = 0;
+ 
+            while (!heap.empty()) {
+                int nod = heap.top().second; // luam nodul din varful heapului si il scoatem, iar daca nu e vizitat luam toate nodurile care pleaca din el
+                heap.pop();
+                if (!vizitat[nod]) {
+                    vizitat[nod] = 1;
+                    for (int i = 1; i < listaCosturi[nod].size(); i++)
+                        if (distanta[nod] + listaCosturi[nod][i].second < distanta[listaCosturi[nod][i].first]) { // verificam distanta pana la nodul curent + cost pentru nod adiacent < distanta pana la nodul adiacent si actualizam
+                            distanta[listaCosturi[nod][i].first] = distanta[nod] + listaCosturi[nod][i].second;
+                            heap.push(make_pair(distanta[listaCosturi[nod][i].first], listaCosturi[nod][i].first)); // punem in heap distanta noua pana la nodul adiacent + nodul adiacent
+                        }
+                }
+            }
+            return distanta;
+        }
+
+        void bellmanFord(){
+            vector<vector<pair<int, int>>> listaCosturi(nrNoduri + 1, vector<pair<int, int>>(1, {-1, -1}));
+            for (int i = 1; i <= nrMuchii; i++) {
+                listaCosturi[listaMuchii[i].second.first].push_back(make_pair(listaMuchii[i].second.second, listaMuchii[i].first));
+            }
+            vector<int> distanta(nrNoduri + 1, inf);
+            vector<int> vizitat(nrNoduri + 1, 0);
+            vector<int> inCoada(nrNoduri + 1, 0); // verif daca un nod se afla in coada deja (altfel se incrementeaza counterul de mai multe ori pe acelasi pas si strica edtectia ciclului)
+
+            queue<int> coada;
+
+            bool areCiclu = false;
+
+            coada.push(1); //punem in coada nodul 1
+            inCoada[1] = 1;
+            distanta[1] = 0;
+
+            while (!coada.empty() && !areCiclu){
+                int nod = coada.front();
+                coada.pop();
+                inCoada[nod] = 0;
+
+                for (int i = 1; i < listaCosturi[nod].size(); i++){
+                    if (distanta[nod] + listaCosturi[nod][i].second < distanta[listaCosturi[nod][i].first]){
+                        distanta[listaCosturi[nod][i].first] = distanta[nod] + listaCosturi[nod][i].second;
+                        vizitat[listaCosturi[nod][i].first]++;
+
+                        if(vizitat[listaCosturi[nod][i].first] >= nrNoduri){
+                            //daca am vizitat de mai mult de nrNoduri ori acelasi nod avem ciclu negativ
+                            areCiclu = true;
+                            break;
+                        }
+
+                        if (!inCoada[listaCosturi[nod][i].first]){
+                            coada.push(listaCosturi[nod][i].first);
+                            inCoada[listaCosturi[nod][i].first] = 1;
+                        }
+                            
+
+                    }
+                        
+                }
+            }
+
+            if (areCiclu)
+                fout << "Ciclu negativ!";
+            else
+                for (int i = 2; i <= nrNoduri; i++)
+                    fout << distanta[i] << " ";
+        }
+
 };
 
 int main(){
-    // int n, m;
-	// vector< pair<int, int> > componente;
-	// fin >> n >> m;
-	// Graf graph(n, m, false);
-	// graph.setareGraf();
-	// componente = graph.muchiiCritice();
-    // for (auto i : componente)
-    //     cout<<i.first<< " "<< i.second << endl;
+    int n, m;
+    fin>>n>>m;
+    Graf graf(n,m,true,true);
+    graf.setareGraf();
+    graf.bellmanFord();
+    fin.close();
+    fout.close();
     return 0;
 }
